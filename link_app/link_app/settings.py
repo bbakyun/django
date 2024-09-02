3a8b4b5f5f4f8e3a84067a28fb974f45096f9f80
bbakyun <guswns0429@gmail.com>
2024-09-02 22:50:49 +0900

    Update settings.py

--- link_app/link_app/settings.py
+++ link_app/link_app/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
