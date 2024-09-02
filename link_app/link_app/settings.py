b6399e973e47d32ee4addebab129a90510f33a0c
bbakyun <guswns0429@gmail.com>
2024-09-02 23:07:56 +0900

    Update settings.py

--- link_app/link_app/settings.py
+++ link_app/link_app/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-z8uq2hh0)u^sv-4%fm0x(n=5_x$eq+t0)ab6!*_9kmtcfv(o5x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
